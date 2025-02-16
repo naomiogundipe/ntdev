d39885a77384d22edfb5273a796893b519a9eef8
naomiogundipe <opemipogundipe@gmail.com>
2025-02-16 13:43:50 +0100

    deploying

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwincss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwincss()]
+  plugins: [react(), tailwincss()],
+  base:"/ntdev"
 });
