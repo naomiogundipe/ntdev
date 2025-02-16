2a5ad1b13834008fc8cd7aebebdffd9fc9beeed7
naomiogundipe <opemipogundipe@gmail.com>
2025-02-16 15:45:28 +0100

    making changes to card

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./Components/Card/Card"
 function App() {
   return (
    <>
-   <div className="flex items-center justify-center w-full h-screen">
+   <div className="flex items-center justify-center w-full min-h-screen bg-[#111a3b]">
    <Card/>
    </div>
    </>
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,15 +3,12 @@ import ImageHolder from "./ImageHolder";
 import CardText from "./CardText";
 import { FaRegMoon, FaRegSun } from "react-icons/fa";
 
-
 const Card = () => {
   const [theme, setTheme] = useState("light");
-
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme") || "light";
     setTheme(savedTheme);
   }, []);
-
   useEffect(() => {
     const root = document.documentElement;
     if (theme === "dark") {
@@ -21,7 +18,6 @@ const Card = () => {
     }
     localStorage.setItem("theme", theme);
   }, [theme]);
-
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
@@ -29,12 +25,12 @@ const Card = () => {
   return (
     <>
       <div
-        className={`lg:w-3/4 lg:h-3/4 h-full bg-[#111a3b] transition-all duration-300  justify-around items-center ${
+        className={`lg:w-3/4 lg:h-3/4 h-full bg-[#111a3b] transition-all duration-300  justify-around items-center  ${
           theme === "dark" ? "dark:bg-slate-400" : ""
         } lg:shadow-md lg:shadow-black w-full lg:rounded-4xl flex flex-col lg:flex-row relative`}
       >
-        <ImageHolder theme={theme}/>
-        <CardText theme={theme}/>
+        <ImageHolder theme={theme} />
+        <CardText theme={theme} />
         <button
           className={`absolute top-[5%] left-[2%] text-5xl text-white ${
             theme === "dark" ? "dark:text-[#111a3b]" : ""
